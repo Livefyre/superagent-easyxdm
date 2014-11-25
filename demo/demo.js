9abe57c7d77c0bb991d50a06c58186d7c91d5567
Benjamin Goering <bengoering@gmail.com>
2014-11-24 22:08:49 -0800

    Update demo to work

--- demo/demo.js
+++ demo/demo.js
@@ -1,20 +1,48 @@
 var request = require('superagent');
 var superAgentEasyXDM = require('..')
 
-request.get('./api')
-.use(superAgentEasyXDM())
-.end(function (err, res) {
-  console.log('made req', res.body);
-  console.log('saeasyxdm is', superAgentEasyXDM);
+var easyXDM = window.easyXDM = require('easyxdm');
 
+var iframeRpc = new easyXDM.Rpc({
+  remote: "./easyxdm.html"
+},
+{
+  remote: {
+    request: {}
+  }
+});
+
+window.iframeRpc = iframeRpc;
+
+iframeRpc.request({
+  url: './api',
+  method: 'get'
+}, onRequestSuccess, function (err) {
+  console.error('req error', err);
+})
+
+function onRequestSuccess(res) {
+  console.log('success req!', arguments);
+  renderJSON(JSON.parse(res.data))
+}
+
+// request.get('./api')
+// .use(superAgentEasyXDM())
+// .end(function (err, res) {
+//   console.log('made req', res.body);
+//   console.log('saeasyxdm is', superAgentEasyXDM);
+//   renderJSON(req.body)
+// })
+
+function renderJSON(json) {
   var pre = document.createElement('pre');
   pre.appendChild(
     document.createTextNode(
-      JSON.stringify(res.body, null, 2)
+      JSON.stringify(json, null, 2)
     )
   );
 
-  document.body.appendChild(pre);
-})
+  document.body.appendChild(pre);  
+}
 
 console.log("Loaded demo.js");
